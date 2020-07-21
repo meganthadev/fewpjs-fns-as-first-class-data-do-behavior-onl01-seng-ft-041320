d93eb5789ecd70e1bb13738756ff4fc512ff1b9c
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 07:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function handleClick(e) {
 function greet(time)
   let time = time.split(:)
   let another_time = this_time[0].concat(this_time[1])
-  let result = parseInt(another_time)
+  let result = parseInt(another_time, 10)
 /* Write your implementation of displayMessage() */
