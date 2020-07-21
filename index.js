8e82174e310f7581d7daa8a68138de3808116ba5
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 07:46:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-function greet(time)
+function greet(time){
   let time = time.split(:)
   let another_time = this_time[0].concat(this_time[1])
   let result = parseInt(another_time, 10)
