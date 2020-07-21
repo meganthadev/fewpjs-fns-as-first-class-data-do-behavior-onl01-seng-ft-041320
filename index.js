431a695e0f03114505fed75ae9ac785153d51994
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 07:33:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(time)
+  
 /* Write your implementation of displayMessage() */
